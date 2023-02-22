1715fb203b04416c85a022355dcbc85f7929a963
SerhiiFront-end <aorl9048@gmail.com>
2023-02-22 00:40:12 +0000

    create express app

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let a = "15"
-console.log(a)
-console.log(a)
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("Hello")
+})
+
+app.listen("3333", (err) => {
+	if (err) {
+		return console.log(err)
+	}
+	console.log("Server is working!")
+})
